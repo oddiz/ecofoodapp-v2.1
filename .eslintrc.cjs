c343aec5994e4dfe1dce6758b6a7a14644b2b429
oddiz <kaansarkaya@gmail.com>
2025-03-04 01:31:02 +0300

    i dunno

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,8 @@
-/** @type {import("eslint").Linter.Config} */
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
@@ -37,4 +35,4 @@ const config = {
     ],
   },
 };
-module.exports = config;
+export default config;
