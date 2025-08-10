85f4702fd422823fb6757637ede422bdf181f78d
Kaan Şarkaya <kaansarkaya@gmail.com>
2025-08-10 21:06:09 +0300

    test: set up Jest with initial utilities test

--- src/utils/sanitizeUrl.ts
+++ src/utils/sanitizeUrl.ts
@@ -1,5 +1,5 @@
 // sanitize the server ip address from / and http
 
 export function sanitizeUrl(url: string): string {
-  return url.replace(/\/|http:|https:/g, "");
+  return url.replace(/^https?:\/\//, "").replace(/\//g, "");
 }
